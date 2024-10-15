7f441aadee59009ec59b31bdb53a50ab460b56d6
bretheskevin <bretheskevin@gmail.com>
2024-10-15 12:44:18 +0200

    fix link

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -21,7 +21,7 @@ export function Navbar() {
 
   return (
     <header className="px-4 lg:px-6 h-14 flex items-center border-b fixed top-0 left-0 right-0 bg-white">
-      <Link className="flex items-center justify-center" href="/public">
+      <Link className="flex items-center justify-center" href="/">
         <Headphones className="h-6 w-6" />
         <span className="block sm:hidden ml-2 text-lg font-semibold">SDPM</span>
         <span className="hidden sm:inline ml-2 text-lg font-semibold">
