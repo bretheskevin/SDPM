8d93c93503efce562773ab0fb6f115757faa5b1d
Kévin Brèthes <bretheskevin@gmail.com>
2024-10-18 13:12:28 +0200

    profile card

--- app/[locale]/profile/ProfileCard.tsx
+++ app/[locale]/profile/ProfileCard.tsx
@@ -9,9 +9,9 @@ interface ProfileCardProps {
 export default function ProfileCard({ profileData }: ProfileCardProps) {
   return (
     <Card
-      className="mb-8 overflow-hidden bg-profile-banner"
+      className="mb-8"
       style={{
-        backgroundImage: `url(${profileData.visuals?.visuals[0]?.visual_url})`,
+        backgroundImage: `url(${profileData.visuals?.visuals[0]?.visual_url}), linear-gradient(315deg, rgb(196, 204, 173) 0%, rgb(125, 118, 107) 100%)`,
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
@@ -25,11 +25,11 @@ export default function ProfileCard({ profileData }: ProfileCardProps) {
             />
             <AvatarFallback>{profileData.full_name.charAt(0)}</AvatarFallback>
           </Avatar>
-          <div className="ml-6 p-4 bg-background/80 rounded-lg">
-            <h1 className="text-3xl font-bold mb-2 text-primary">
+          <div className="ml-6 p-4 bg-background/90 rounded-lg">
+            <h1 className="text-3xl font-bold mb-2 text-foreground">
               {profileData.full_name}
             </h1>
-            <p className="text-xl text-primary/80 mb-2">
+            <p className="text-xl text-foreground/80 mb-2">
               @{profileData.username}
             </p>
             <div className="flex flex-wrap gap-2 mb-4">
@@ -39,14 +39,16 @@ export default function ProfileCard({ profileData }: ProfileCardProps) {
               )}
               {profileData.badges.verified && <Badge>Verified</Badge>}
             </div>
-            <p className="text-sm text-primary/80">
+            <p className="text-sm text-foreground/80">
               {profileData.city}, {profileData.country_code}
             </p>
           </div>
         </div>
         {profileData.description && (
-          <div className="mt-4 p-4 bg-background/80 rounded-lg">
-            <p className="text-sm text-primary/80">{profileData.description}</p>
+          <div className="mt-4 p-4 bg-background/90 rounded-lg">
+            <p className="text-sm text-foreground/80">
+              {profileData.description}
+            </p>
           </div>
         )}
       </CardContent>
