41d71ce4af51261ba5d59f4f082e4f68e0950a67
bretheskevin <bretheskevin@gmail.com>
2024-10-29 16:36:23 +0100

    feat(privacy_page): reduced text size

--- app/[locale]/privacy/page.tsx
+++ app/[locale]/privacy/page.tsx
@@ -13,14 +13,14 @@ export default async function PrivacyPage() {
         <div className="mb-6 inline-flex h-16 w-16 items-center justify-center rounded-full bg-primary/10 sm:mb-8 sm:h-20 sm:w-20">
           <Shield className="h-8 w-8 text-primary sm:h-10 sm:w-10" />
         </div>
-        <h1 className="mb-4 text-3xl font-bold sm:mb-6 sm:text-4xl">{scopedT("title")}</h1>
+        <h1 className="mb-4 text-2xl font-bold sm:mb-6 sm:text-3xl">{scopedT("title")}</h1>
         <div className="prose dark:prose-invert mx-auto">
-          <p className="mb-4 text-lg text-gray-600 dark:text-gray-300 sm:mb-6 sm:text-xl">{scopedT("description")}</p>
-          <h2 className="mb-4 text-2xl font-semibold sm:mb-6 sm:text-3xl">{scopedT("weDoNotCollectAnyData")}</h2>
+          <p className="mb-4 text-base text-gray-600 dark:text-gray-300 sm:mb-6 sm:text-lg">{scopedT("description")}</p>
+          <h2 className="mb-4 text-xl font-semibold sm:mb-6 sm:text-2xl">{scopedT("weDoNotCollectAnyData")}</h2>
           <div className="mb-6 rounded-lg border border-primary/10 bg-primary/5 p-4 sm:mb-8 sm:p-6">
             <p className="mb-3 text-base sm:mb-4 sm:text-lg">{scopedT("weDoNotCollectAnyDataDescription")}</p>
             <p className="mb-3 text-base sm:mb-4 sm:text-lg">{scopedT("whenUsing")}</p>
-            <ul className="mb-3 list-inside list-disc text-left text-base sm:mb-4 sm:text-lg">
+            <ul className="sm:text-ld mb-3 list-inside list-disc text-left text-base sm:mb-4">
               {["noPersonalInformation", "noUsageData", "noCookies", "noDataShared"].map((item, index) => (
                 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
                 // @ts-expect-error
